1f66e3ec4153d34757c03e3b0a3bd6825c870b73
Geoffrey Yeung <yeungchingho123@gmail.com>
2020-11-26 18:28:45 +0800

    add state to twtich auth

--- auth.js
+++ auth.js
@@ -19,6 +19,8 @@ const twitch_client_id = process.env.TWITCH_CLIENT_ID;
 const twitch_client_secret = process.env.TWITCH_CLIENT_SECRET;
 const domain = process.env.DOMAIN || "http://localhost:5000";
 
+const { v4: uuidv4 } = require('uuid');
+
 app.get("/", function (req, res) {
     res.send(`Hello World!`);
 });
@@ -29,18 +31,108 @@ app.get("/register", function (req, res) {
 
 app.get("/register/full", function (req, res) {
     console.log('Redirecting to register full...');
-    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/redirect/full&response_type=code&scope=chat:read+chat:edit+channel:moderate`);
+    const state = uuidv4();
+    redis_client.set(state, 1, 'EX', 5 * 60);
+    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/redirect/full&response_type=code&scope=chat:read+chat:edit+channel:moderate&state=${state}`);
 });
 
 app.get("/register/lite", function (req, res) {
     console.log('Redirecting to register lite...');
-    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/redirect/lite&response_type=code&scope=chat:read+chat:edit`);
+    const state = uuidv4();
+    redis_client.set(state, 1, 'EX', 5 * 60);
+    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/redirect/lite&response_type=code&scope=chat:read+chat:edit&state=${state}`);
 });
 
 function get_redirect_func(plan_type) {
     return async function (req, res) {
+        const state = req.query.state;
+        if (!state) {
+            res.send("Invalid request.");
+            return;
+        }
+        redis_client.get(state, async (err, reply) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(state, reply);
+            if (reply === null) {
+                res.send('Invalid state. Probably the code expired. Try unregistering again.')
+                return;
+            }
+
+            redis_client.del(state);
+            const code = req.query.code;
+            console.log('registering - received code:', code);
+            const token_data = await axios.post("https://id.twitch.tv/oauth2/token", null,
+                {
+                    params: {
+                        "client_id": twitch_client_id,
+                        "client_secret": twitch_client_secret,
+                        "code": code,
+                        "grant_type": "authorization_code",
+                        "redirect_uri": `${domain}/redirect/full`
+                    }
+                }).then(r => r.data).catch(err => console.error(err));
+            console.log(token_data);
+            const user_data = await axios.get("https://api.twitch.tv/helix/users?",
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token_data['access_token']}`,
+                        "Client-Id": twitch_client_id
+                    }
+                })
+                .then(r => r.data)
+                .then(r => r.data[0])
+                .catch(err => console.error(err));
+            console.log(user_data);
+            user_data.id = parseInt(user_data.id, 10);
+
+            const db_client = await db_pool.connect();
+            var expiry = new Date();
+            expiry.setSeconds(expiry.getSeconds() + token_data.expires_in);
+            // console.log([user_data.id, user_data.login, token_data.access_token, token_data.refresh_token, expiry]);
+            await db_client.query("INSERT INTO access_tokens VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT ON CONSTRAINT access_tokens_pkey DO UPDATE SET access_token = $3, refresh_token = $4, expiry = $5, plan_type = $6", [user_data.id, user_data.login, token_data.access_token, token_data.refresh_token, expiry, plan_type]);
+            db_client.release();
+
+            redis_client.publish("launch", user_data.id);
+
+            res.send(`${user_data.display_name}, you have been registered!\nPlan type: ${plan_type}`);
+        })
+
+    };
+}
+
+app.get("/redirect/full", get_redirect_func('full'));
+app.get("/redirect/lite", get_redirect_func('lite'));
+
+app.get("/unregister", async function (req, res) {
+    console.log('Redirecting to unregister...');
+    const state = uuidv4();
+    redis_client.set(state, 1, 'EX', 5 * 60);
+    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/unregister/confirm&response_type=code&scope=&force_verify=true&state=${state}`);
+})
+
+app.get("/unregister/confirm", async function (req, res) {
+    const state = req.query.state;
+    if (!state) {
+        res.send("Invalid request.");
+        return;
+    }
+    redis_client.get(state, async (err, reply) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(state, reply);
+        if (reply === null) {
+            res.send('Invalid state. Probably the code expired. Try unregistering again.')
+            return;
+        }
+
+        redis_client.del(state);
         const code = req.query.code;
-        console.log('registering - received code:', code);
+        console.log('unregistering - received code:', code);
         const token_data = await axios.post("https://id.twitch.tv/oauth2/token", null,
             {
                 params: {
@@ -48,7 +140,7 @@ function get_redirect_func(plan_type) {
                     "client_secret": twitch_client_secret,
                     "code": code,
                     "grant_type": "authorization_code",
-                    "redirect_uri": `${domain}/redirect/full`
+                    "redirect_uri": `${domain}/unregister/confirm`
                 }
             }).then(r => r.data).catch(err => console.error(err));
         console.log(token_data);
@@ -63,60 +155,14 @@ function get_redirect_func(plan_type) {
             .then(r => r.data[0])
             .catch(err => console.error(err));
         console.log(user_data);
-        user_data.id = parseInt(user_data.id, 10);
-
+        const user_id = parseInt(user_data.id, 10);
         const db_client = await db_pool.connect();
-        var expiry = new Date();
-        expiry.setSeconds(expiry.getSeconds() + token_data.expires_in);
-        // console.log([user_data.id, user_data.login, token_data.access_token, token_data.refresh_token, expiry]);
-        await db_client.query("INSERT INTO access_tokens VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT ON CONSTRAINT access_tokens_pkey DO UPDATE SET access_token = $3, refresh_token = $4, expiry = $5, plan_type = $6", [user_data.id, user_data.login, token_data.access_token, token_data.refresh_token, expiry, plan_type]);
+        const db_res = await db_client.query("DELETE FROM access_tokens WHERE twitch_id = $1", [user_id]);
         db_client.release();
+        redis_client.publish("kill", user_id);
+        res.send(`${user_data.display_name} (id ${user_id}), you have been unregistered.`);
+    });
 
-        redis_client.publish("launch", user_data.id);
-
-        res.send(`${user_data.display_name}, you have been registered!\nPlan type: ${plan_type}`);
-    };
-}
-
-app.get("/redirect/full", get_redirect_func('full'));
-app.get("/redirect/lite", get_redirect_func('lite'));
-
-app.get("/unregister", async function (req, res) {
-    console.log('Redirecting to unregister...');
-    res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${twitch_client_id}&redirect_uri=${domain}/unregister/confirm&response_type=code&scope=&force_verify=true`);
-})
-
-app.get("/unregister/confirm", async function (req, res) {
-    const code = req.query.code;
-    console.log('unregistering - received code:', code);
-    const token_data = await axios.post("https://id.twitch.tv/oauth2/token", null,
-        {
-            params: {
-                "client_id": twitch_client_id,
-                "client_secret": twitch_client_secret,
-                "code": code,
-                "grant_type": "authorization_code",
-                "redirect_uri": `${domain}/unregister/confirm`
-            }
-        }).then(r => r.data).catch(err => console.error(err));
-    console.log(token_data);
-    const user_data = await axios.get("https://api.twitch.tv/helix/users?",
-        {
-            headers: {
-                "Authorization": `Bearer ${token_data['access_token']}`,
-                "Client-Id": twitch_client_id
-            }
-        })
-        .then(r => r.data)
-        .then(r => r.data[0])
-        .catch(err => console.error(err));
-    console.log(user_data);
-    const user_id = parseInt(user_data.id, 10);
-    const db_client = await db_pool.connect();
-    const db_res = await db_client.query("DELETE FROM access_tokens WHERE twitch_id = $1", [user_id]);
-    db_client.release();
-    redis_client.publish("kill", user_id);
-    res.send(`${user_data.display_name} (id ${user_id}), you have been unregistered.`);
 })
 
 var port = process.env.PORT || 5000;
